83989963e071eaede14f5b7e291d72e091fe6e84
pluto <2064623160@qq.com>
2023-05-28 10:28:38 +0800

    sm icon

--- src/components/editor/ErComponent.tsx
+++ src/components/editor/ErComponent.tsx
@@ -1,18 +1,22 @@
 import "@/sass/editor/ErComponent.scss";
 import { ErConfig as erConfig } from "../../utils/ErComponent-config";
 import { defineComponent, reactive, ref } from "vue";
-import { ElInput, ElSwitch } from "element-plus";
+import { ElInput, ElInputNumber, ElSwitch } from "element-plus";
 export default defineComponent({
-  setup() {
+  props: {
+    modelValue: { type: Object },
+  },
+  setup(props) {
     interface btn {
       label: String; // 标签
       active: boolean; // 是否被选择了
+      isShow: boolean; // 是否显示
     }
-
+    // 切换菜单
     const buttons: btn[] = reactive([
-      { label: "属性", active: true },
-      { label: "外观", active: false },
-      { label: "事件", active: false },
+      { label: "属性", active: true, isShow: true },
+      { label: "外观", active: false, isShow: true },
+      { label: "事件", active: false, isShow: true },
     ]);
 
     // 选择按钮的点击事件selectBtn
@@ -25,37 +29,63 @@ export default defineComponent({
         button.active = false;
       });
     };
-    console.log(erConfig.componentList);
+
+    // 普通输入框
+    const inputValue = ref("");
 
     // 下拉器
     const activeNames: string[] = ["1", "2"];
-    // 按钮
-    const isOpen = ref(true);
-    // 选择器
-    const selectValue = ref("");
 
-    const options = [
+    // switch按钮
+    const isOpen = ref(true);
+
+    // 选择器
+    // value: "icon iconfont icon-xiaolian",
+    let selectValue = ref(""); // 记录选择的内容
+    let selectIndex = ref(-1);
+    const options = reactive([
       {
-        value: "Option1",
-        label: "Option1",
+        value: "icon iconfont icon-xiaolian",
+        label: "icon-xiaolian",
+        selected: false,
+        render: () => {
+          return <span class="icon iconfont icon-xiaolian"></span>;
+        },
       },
       {
-        value: "Option2",
-        label: "Option2",
+        value: "icon iconfont icon-xiaolian",
+        label: "icon-xiaolian",
+        selected: false,
+        render: () => {
+          return <span class="icon iconfont icon-xiaolian"></span>;
+        },
       },
-      {
-        value: "Option3",
-        label: "Option3",
-      },
-      {
-        value: "Option4",
-        label: "Option4",
-      },
-      {
-        value: "Option5",
-        label: "Option5",
-      },
-    ];
+    ]);
+
+    const changeSelectValue = (value: any,index:number): void => {
+      selectValue.value = value.label;
+      options.map((option) => {
+        option.selected = false;
+      });
+      value.selected = true; // 标记选中的图标
+      selectIndex.value = index;
+    };
+
+    const clearIcon = (): void => {
+      selectValue.value = "";
+      options.map((option) => {
+        option.selected = false;
+      });
+    }; // 清除选择的图标内容
+    // padding
+    let isActive = ref("middle");
+    const setPadding = (pos: string): void => {
+      isActive.value = pos;
+    };
+    const padValue = ref(0); // 记录输入的padding值
+    const handlePadValueChange = (padValue: number): void => {
+      console.log(padValue);
+    };
 
     return () => {
       return (
@@ -64,6 +94,7 @@ export default defineComponent({
             {buttons.map((button, index) => {
               return (
                 <label
+                  v-show={button.isShow}
                   onClick={() => selectBtn(index)}
                   class={[
                     button.active ? "ErComponent-nav-active" : "",
@@ -77,10 +108,16 @@ export default defineComponent({
           </div>
           <elCollapse v-model={activeNames}>
             <elCollapseItem title="基本" name="1">
+              {/* 输入框 */}
               <div class="elCollapseItem elCollapseInput">
                 <div class="elCollapseInput-name">名称</div>
-                <ElInput></ElInput>
+                <ElInput
+                  modelValue={inputValue.value}
+                  placeholder="请输入名称"
+                  clearable
+                ></ElInput>
               </div>
+              {/* switch按钮 */}
               <div class="elCollapseItem elCollapseButton">
                 <div class="elCollapseButton-name">二次确认</div>
                 {/* <ElButton></ElButton> */}
@@ -89,22 +126,98 @@ export default defineComponent({
                   style="--el-switch-on-color: #2468f2;"
                 ></ElSwitch>
               </div>
-              <div class="elCollapseItem elCollapseInput">
-                <div class="elCollapseInput-name">左侧图标</div>
-                <el-select
-                  v-model={selectValue}
-                  class="m-2"
-                  placeholder="点击选择图标"
-                  size="small"
-                >
-                  {options.map((item) => (
-                    <el-option
-                      key={item.value}
-                      label={item.value}
-                      value={item.value}
-                    ></el-option>
-                  ))}
-                </el-select>
+              {/* 选择器 */}
+              <div class="elCollapseItem elCollapseSelect">
+                <div class="elCollapseSelect-name">左侧图标</div>
+                <div class="elCollapseSelect-select">
+                  <el-select
+                    modelValue={selectValue.value}
+                    class="m-2"
+                    placeholder="点击选择图标"
+                    size="small"
+                    clearable
+                    clear-icon={() => (
+                      <span
+                        class="icon iconfont icon-xiaolian"
+                        onClick={clearIcon}
+                      ></span>
+                    )}
+                  >
+                    {(options || []).map((item, index) => (
+                      <el-option
+                        class={[item.value, item.selected ? "active" : ""]}
+                        key={index}
+                        // 将字符串转为标签 (new DOMParser).parseFromString(selectValue.value,"text/html").body
+                        label={selectValue.value ? selectValue.value : null}
+                        value=""
+                        onClick={(): void => changeSelectValue(item,index)}
+                      ></el-option>
+                    ))}
+                  </el-select>
+                </div>
+              </div>
+              {/* padding */}
+              <div class="elCollapseItem title">边框</div>
+              <div class="elCollapseItem elCollapsePad">
+                <div class="elCollapsePad-icon">
+                  <div></div>
+                  <div class="common">
+                    <div
+                      class={[
+                        "top",
+                        isActive.value == "top" ? "active-top" : "",
+                      ]}
+                      onClick={() => setPadding("top")}
+                    ></div>
+                  </div>
+                  <div></div>
+                  <div class="common">
+                    <div
+                      class={[
+                        "left",
+                        isActive.value == "left" ? "active-left" : "",
+                      ]}
+                      onClick={() => setPadding("left")}
+                    ></div>
+                  </div>
+                  <div class="common">
+                    <div
+                      class={[
+                        "middle",
+                        isActive.value == "middle" ? "active-middle" : "",
+                      ]}
+                      onClick={() => setPadding("middle")}
+                    ></div>
+                  </div>
+                  <div class="common">
+                    <div
+                      class={[
+                        "right",
+                        isActive.value == "right" ? "active-right" : "",
+                      ]}
+                      onClick={() => setPadding("right")}
+                    ></div>
+                  </div>
+                  <div></div>
+                  <div class="common">
+                    <div class="bottom"></div>
+                    <div
+                      class={[
+                        "bottom",
+                        isActive.value == "bottom" ? "active-bottom" : "",
+                      ]}
+                      onClick={() => setPadding("bottom")}
+                    ></div>
+                  </div>
+                  <div></div>
+                </div>
+                <div class="elCollapsePad-setting">
+                  <ElInputNumber
+                    modelValue={padValue.value}
+                    onChange={handlePadValueChange}
+                  ></ElInputNumber>
+                  <div class="lineAndColor"></div>
+                </div>
               </div>
               {/* {erConfig.componentList.map((component) => component.render())} */}
             </elCollapseItem>
