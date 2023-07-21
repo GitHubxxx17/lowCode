bdbeab8fef141a2f12b312430ee6d6576693934e
pluto <2064623160@qq.com>
2023-07-21 20:38:30 +0800

    feat: 登陆注册

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import Editor from './views/editor.vue';
+// import Editor from "./views/editor.vue";
+// import Login from "./views/login.vue";
 </script>
 
 <template>
- <Editor></Editor>
+  <!-- <Editor></Editor> -->
+  <router-view></router-view>
+  <!-- <Login></Login> -->
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
