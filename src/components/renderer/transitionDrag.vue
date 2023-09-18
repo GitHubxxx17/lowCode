b4061df1e3f962d0d8525e84bb5255dfb12d1d42
“Githubxxx17” <s486275391@outlook.com>
2023-09-18 23:20:53 +0800

    fix:减少一层嵌套

--- src/components/renderer/transitionDrag.vue
+++ src/components/renderer/transitionDrag.vue
@@ -12,21 +12,16 @@ const isContainer = (key: string): boolean => {
 </script>
 <!-- 利用transition-group绑定key和vue渲染机制实现拖拽动画，只需要修改渲染数据的顺序即可刷新dom节点实现拖拽动画移动 -->
 <template>
-  <transition-group
-    move-class="move"
-    class="transition"
-    tag="div"
-  >
+  <transition-group move-class="move" class="transition" tag="div">
     <template
       v-for="item in mainData.EditorDataMap.get(props.dataKey).children"
       :key="item"
     >
-      <div class="cannotPreview" v-if="!isContainer(item)" :data-key="item">
-        <RenderNode
-          :dataKey="item"
-          :EditorDataMap="mainData.EditorDataMap"
-        ></RenderNode>
-      </div>
+      <RenderNode
+        :dataKey="item"
+        :EditorDataMap="mainData.EditorDataMap"
+        v-if="!isContainer(item)"
+      ></RenderNode>
       <RenderNode
         v-else
         :dataKey="item"
@@ -34,7 +29,6 @@ const isContainer = (key: string): boolean => {
       ></RenderNode>
     </template>
   </transition-group>
-  
 </template>
 
 <style scoped>
